b1c36f388586ec6554169d066a64356e428d094d
anishamroun <anishamroun2@gmail.com>
2019-01-30 00:08:09 +0200

    stable version

--- MovieAndMe/App.js
+++ MovieAndMe/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import Search from './Components/Search';
-import FlexComponents from './Components/FlexComponents';
+// App.js
+
+import React from 'react'
+import Navigation from './Navigation/Navigation'
+//import Search from './Components/Search'
 
 export default class App extends React.Component {
   render() {
     return (
-      <Search/>
+     // <Search/>
+      <Navigation/>
+     
+    )
+  }
+}
 
+/**
+
+import React from "react";
+import { View, Text } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Home Screen</Text>
+      </View>
     );
   }
 }
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  }
+});
+
+export default createAppContainer(AppNavigator);
+
+ */
