7be97df1db1afa1c0967542062afed02d970fcd4
anishamroun <anishamroun2@gmail.com>
2019-02-10 22:32:45 +0200

    Component lifecycle

--- MovieAndMe/Components/FilmDetail.js
+++ MovieAndMe/Components/FilmDetail.js
@@ -1,15 +1,86 @@
 // Components/FilmDetail.js
 
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Image, ActivityIndicator, ScrollView } from 'react-native'
+import { getFilmDetailFromApi, getImageFromApi } from '../API/TMDBApi'
+
+import moment from 'moment'
+import numeral from 'numeral'
 
 class FilmDetail extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      film: undefined,
+      isLoading: true
+    }
+  }
+
+  componentDidMount() {
+    getFilmDetailFromApi(this.props.navigation.state.params.idFilm).then(data => {
+      this.setState({
+        film: data,
+        isLoading: false
+      })
+    })
+  }
+
+  _displayLoading() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.loading_container}>
+          <ActivityIndicator size='large' />
+        </View>
+      )
+    }
+  }
+
+  _displayFilm() {
+    
+    if (this.state.film != undefined) {
+      return (
+        <ScrollView style={styles.scrollview_container}>
+          <Image style={styles.image} 
+                 source={{uri : getImageFromApi(this.state.film.backdrop_path)}}
+          />
+          <Text style={styles.title_text}>{this.state.film.title}</Text>
+          
+          
+          <Text style={styles.description_text}>
+                        {this.state.film.overview}</Text>
+          <Text style={styles.default_text}>Sorti le 
+                        {moment(this.state.film.release_date).format("DD/MM/YYYY")} </Text>
+          <Text style={styles.default_text}>Note : 
+                        {this.state.film.vote_average}/10</Text>
+          
+          <Text style={styles.default_text}>Nombre de votes : 
+                        {this.state.film.vote_count}</Text>
+          
+          <Text style={styles.default_text}>Budget : 
+                        {numeral(this.state.film.budget).format("0,0[.]00 $")}</Text>
+          
+          <Text style={styles.default_text}>Budget : 
+                        {numeral(this.state.film.budget).format("0,0[.]00 $")}</Text>
+          <Text style={styles.default_text}>Genre(s) : 
+                        {this.state.film.genres.map(function(genre){
+                                  return genre.name; }).join(" / ")}
+                                  </Text>
+          <Text style={styles.default_text}>Companie(s) : 
+                        {this.state.film.production_companies.map(
+                          function(company){
+                            return company.name; 
+                            }).join(" / ")}
+            </Text>
+        </ScrollView>
+      )
+    }
+  }
 
   render() {
-    const idFilm = this.props.navigation.state.params.idFilm
     return (
       <View style={styles.main_container}>
-        <Text>Detail du film { idFilm }</Text>
+        {this._displayLoading()}
+        {this._displayFilm()}
       </View>
     )
   }
@@ -17,7 +88,46 @@ class FilmDetail extends React.Component {
 
 const styles = StyleSheet.create({
   main_container: {
+    flex: 1
+  },
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  scrollview_container: {
+    flex: 1
+  },
+  image : {
+    height: 169,
+    margin: 5
+  },
+  title_text: {
+    fontWeight: "bold",
+    fontSize: 35,
     flex: 1,
+    flexWrap: "wrap",
+    marginLeft:5,
+    marginRight:5,
+    marginTop:10,
+    marginBottom:10,
+    color: "#000000",
+    textAlign:"center"
+  },
+  description_text: {
+    fontStyle: "italic",
+    color: "#666666",
+    margin: 5,
+    marginBottom: 15,
+  },
+  default_text : {
+    marginLeft:5,
+    marginRight:5,
+    marginTop:5,
   }
 })
 
--- MovieAndMe/Components/Search.js
+++ MovieAndMe/Components/Search.js
@@ -35,8 +35,6 @@ class Search extends React.Component {
 
 _displayDetailForFilm = (idFilm) => {
 
-  console.log("Display film with id " + idFilm)
-
   this.props.navigation.navigate("FilmDetail", {idFilm: idFilm})
 
 }
